cd1a036ef7065df27873ec622f00a09856e95af9
akka-tech <akkaabdlwahd3@gmail.com>
2025-07-27 21:59:51 +0200

    Add star icon image to public assets

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
-
-export default function Pill() {
+const Pill: React.FC = () => {
   return (
-    <div>P</div>
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">Title</p>
+    </div>
   )
 }
+
+export default Pill;
